2e11c372de949bca1d46e9f3a071a7c4e1b4b14f
alvinrach <alvinracht@gmail.com>
2025-02-23 16:16:03 +0700

    Optimize: Unused modules

--- src/modules.py
+++ src/modules.py
@@ -49,44 +49,8 @@ def get_query_engine(model_name="gemini"):
 
     return query_engine, index, chroma_collection
 
-def rag_query_nonload(query):
-    embed_model = FastEmbedEmbedding(model_name="sentence-transformers/all-MiniLM-L6-v2")
-    llm = Gemini(
-        model="models/gemini-2.0-flash"
-    )
-
-    Settings.llm = llm
-    Settings.embed_model = embed_model
-
-
-    db = chromadb.PersistentClient(path="./chroma_db")
-    chroma_collection = db.get_or_create_collection("seconddoc")
-    vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
-    storage_context = StorageContext.from_defaults(vector_store=vector_store)
-
-    index = VectorStoreIndex.from_vector_store(
-        vector_store=vector_store,
-        storage_context=storage_context,
-        embed_model=embed_model
-    )
-
-    query_engine = index.as_query_engine()
-
-    qa_prompt_tmpl_str = (
-        "Informasi konteks ada di bawah ini.\n"
-        "---------------------\n"
-        "{context_str}\n"
-        "---------------------\n"
-        "Berdasarkan informasi konteks dan bukan pengetahuan sebelumnya, "
-        "jawablah pertanyaan berikut.\n"
-        "Pertanyaan: {query_str}\n"
-        "Jawaban: "
-    )
-    qa_prompt_tmpl = PromptTemplate(qa_prompt_tmpl_str)
-
-    query_engine.update_prompts(
-        {"response_synthesizer:text_qa_template": qa_prompt_tmpl}
-    )
+def get_query_engine_dashboard(query):
+    query_engine, _, _ = get_query_engine()
     response = query_engine.query(query)
 
     res = {
@@ -97,30 +61,3 @@ def rag_query_nonload(query):
 
     print(res)
     return res
-
-def rag_query(query):
-    documents = SimpleDirectoryReader("data").load_data()
-    embed_model = FastEmbedEmbedding(model_name="sentence-transformers/all-MiniLM-L6-v2")
-    llm = Gemini(
-        model="models/gemini-2.0-flash"
-    )
-
-    Settings.llm = llm
-    Settings.embed_model = embed_model
-
-
-    db = chromadb.PersistentClient(path="./chroma_db")
-    chroma_collection = db.get_or_create_collection("seconddoc")
-    vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
-    storage_context = StorageContext.from_defaults(vector_store=vector_store)
-
-    index = VectorStoreIndex.from_vector_store(
-        vector_store=vector_store,
-        storage_context=storage_context,
-        embed_model=embed_model
-    )
-
-    query_engine = index.as_query_engine()
-    response = query_engine.query(query)
-
-    return response.response
