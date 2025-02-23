c2e942a902123a49c0869e62d95702ebd849d3ae
alvinrach <alvinracht@gmail.com>
2025-02-23 12:41:09 +0700

    Evaluation: Notebook and Modules

--- src/modules.py
+++ src/modules.py
@@ -4,10 +4,50 @@ load_dotenv()
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, PromptTemplate
 from llama_index.embeddings.fastembed import FastEmbedEmbedding
 from llama_index.llms.gemini import Gemini
+from llama_index.llms.openai import OpenAI
 from llama_index.core import Settings
 from llama_index.vector_stores.chroma import ChromaVectorStore
 from llama_index.core import StorageContext
 import chromadb
+
+def get_query_engine(model_name="gemini"):
+    embed_model = FastEmbedEmbedding(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    if model_name=="gemini":
+        llm = Gemini(model="models/gemini-2.0-flash")
+    elif model_name=="gpt-4o-mini":
+        llm = OpenAI(model="gpt-4o-mini", temperature=0.1)
+
+    Settings.llm = llm
+    Settings.embed_model = embed_model
+
+    db = chromadb.PersistentClient(path="../chroma_db")
+    chroma_collection = db.get_or_create_collection("seconddoc")
+    vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+
+    index = VectorStoreIndex.from_vector_store(
+        vector_store=vector_store,
+        embed_model=embed_model
+    )
+
+    query_engine = index.as_query_engine()
+
+    qa_prompt_tmpl_str = (
+        "Informasi konteks ada di bawah ini.\n"
+        "---------------------\n"
+        "{context_str}\n"
+        "---------------------\n"
+        "Berdasarkan informasi konteks dan bukan pengetahuan sebelumnya, "
+        "jawablah pertanyaan berikut.\n"
+        "Pertanyaan: {query_str}\n"
+        "Jawaban: "
+    )
+    qa_prompt_tmpl = PromptTemplate(qa_prompt_tmpl_str)
+
+    query_engine.update_prompts(
+        {"response_synthesizer:text_qa_template": qa_prompt_tmpl}
+    )
+
+    return query_engine, index, chroma_collection
 
 def rag_query_nonload(query):
     embed_model = FastEmbedEmbedding(model_name="sentence-transformers/all-MiniLM-L6-v2")
